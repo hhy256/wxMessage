cde957dacf04d78a541575f47cfa0c209e89bd43
hhy256 <62878877+hhy256@users.noreply.github.com>
2023-04-29 21:01:41 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -21,68 +21,46 @@ public class WechatConfig {
 
     public static final List<User> userList = new ArrayList<>();
 
-    /**
-     * 配置用户信息
-     *
-     *如果要多个人的话，就复制这个一遍，然后填写里面的内容。这里默认两个人,大伙应该是两个人吧（笑）
-     *如果开启了master模式，除第一个用户外，其他用户只需要填写微信号
-     * 要计算几个日期，就写几个new BirthDay,第一个在模板中是{{birthDay.DATA}}，第二个是{{birthDay1.DATA}}，第三个是{{birthDay2.DATA}}以此类推
-     * new BirthDay()里面的参数分别代表：
-     * 1. [年]，日期里面的数字，填正常的数字就行了.比如1就是1，不要填01
-     * 2. [月]
-     * 3. [日]
-     * 4. [是否是农历(true 为农历、false 为公历)]
-     * 5. [是否统计天数(true 为统计，false 为倒计时)]
-     * 6. [倒计时到0天提示信息(如果类型为统计可以不填)]
-     * 注意：每个用户信息的最后一项不需要加逗号！！！
-     */
-    static {
-        userList.add(getUser(
-                "这个人扫码后的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
-                "南京", //这个人在的城市
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
-                new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false,"周年快乐！！！")
-        ));
-
-        userList.add(getUser(
-                "这个人扫码后的微信号",
-                "微信消息模板ID",
-                "这个人的称呼",
-                "江苏省南京市玄武区",
-                "南京",
-                new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false,"周年快乐！！！")
-        ));
-
-
+   package com.mycompany.notification;
+import com.mycompany.notification.pojo.Birthday;
+import com.mycompany.notification.pojo.User;
+import com.mycompany.wechat.WeChatService;
+import com.mycompany.wechat.pojo.TemplateData;
+import com.mycompany.wechat.pojo.TemplateMessage;
+import java.util.ArrayList;
+import java.util.List;
+public class NotificationSender {
+    public static void main(String[] args) {
+        // 获取通知模板的ID、标题、内容和发送者微信号
+        String templateId = NotificationConfig.getTemplateId();
+        String title = NotificationConfig.getTitle();
+        String content = NotificationConfig.getContent();
+        String senderVx = NotificationConfig.getSenderVx();
+        // 获取通知接收者列表
+        List<User> userList = NotificationConfig.getUserList();
+        // 遍历通知接收者列表，向每个用户发送通知
+        for (User user : userList) {
+            // 获取用户的微信号、姓名和城市
+            String userVx = user.getVx();
+            String userName = user.getUserName();
+            String city = user.getCity();
+            // 获取用户的生日信息
+            Birthday[] birthdays = user.getBirthdays();
+            // 构造模板消息
+            TemplateMessage templateMessage = new TemplateMessage();
+            templateMessage.setTemplateId(templateId);
+            templateMessage.setToUser(userVx);
+            templateMessage.setUrl("");
+            List<TemplateData> dataList = new ArrayList<>();
+            // 设置模板消息的各个参数
+            dataList.add(new TemplateData("first", title));
+            dataList.add(new TemplateData("keyword1", userName));
+            dataList.add(new TemplateData("keyword2", city));
+            dataList.add(new TemplateData("keyword3", content.replace("{date}", "2022年10月1日").replace("{time}", "9:00-18:00")));
+            dataList.add(new TemplateData("remark", "祝您生活愉快！"));
+            templateMessage.setDataList(dataList);
+            // 发送模板消息
+            WeChatService.sendTemplateMessage(senderVx, templateMessage);
+        }
     }
-
-    private static User getUser(String vx, String templateId, String username, String address, String city, BirthDay... birthDays){
-        User user=new User();
-        user.setVx(vx);
-        user.setUserName(username);
-        user.setBirthDays(birthDays);
-        user.setAddress(address);
-        user.setCity(city);
-        user.setTemplateId(templateId);
-        return user;
-    }
-    private static User getUser(String vx, String templateId, String username, BirthDay... birthDays){
-        return getUser(vx,templateId,username,null,null,birthDays);
-    }
-    private static User getUser(String vx, String templateId, BirthDay... birthDays){
-        return getUser(vx,templateId,null,null,null,birthDays);
-    }
-    private static User getUser(String vx,BirthDay... birthDays){
-        return getUser(vx,null,null,null,null,birthDays);
-    }
-
-
 }
